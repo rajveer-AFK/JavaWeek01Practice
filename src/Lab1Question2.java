e61972b114e520adbf047ea409e5bae9056e708c
Rajveer Singh <rajveersinghshergill112@gmail.com>
2024-08-04 01:12:55 -0700

    removing redundant code

--- src/Lab1Question2.java
+++ src/Lab1Question2.java
@@ -23,17 +23,12 @@ public class Lab1Question2 {
 		final int YARDS_IN_MILE = 1760;
 		
 		// Getting and storing a user value for number of miles
-		// Change this
-		String user_input;
-		int num_miles;
-		
-		user_input = JOptionPane.showInputDialog(null, "Enter the number of miles");
-		num_miles = Integer.parseInt(user_input);
+		int numMiles = Integer.parseInt(JOptionPane.showInputDialog(null, "Enter the number of miles"));
 		
 		// Outputting the miles in inches, feet, and yards
-		JOptionPane.showMessageDialog(null, num_miles + " miles is equivalent to "
-											+ INCHES_IN_MILE * num_miles + " inches, "
-											+ FEET_IN_MILE * num_miles + " miles, and "
-											+ YARDS_IN_MILE * num_miles + " yards");
+		JOptionPane.showMessageDialog(null, numMiles + " miles is equivalent to "
+											+ INCHES_IN_MILE * numMiles + " inches, "
+											+ FEET_IN_MILE * numMiles + " miles, and "
+											+ YARDS_IN_MILE * numMiles + " yards");
 	}
 }
